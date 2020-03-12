1ca5df7e94045ad246bda9f5c2e30c07456e4e85
André Luis <dolby1games@gmail.com>
2020-03-12 19:51:46 -0300

    Ajuste para exibicao ser mais entendivel

--- Trabalho1103/src/aplicacao/Programa.java
+++ Trabalho1103/src/aplicacao/Programa.java
@@ -12,14 +12,14 @@ public class Programa {
 		System.out.println(oPessoa.toString());
         
         System.out.println("idade pessoa:" + oPessoa.GetIdadePessoa());
-		System.out.println("Data válida:" + oPessoa.DataValida("01/01/2000"));
-		System.out.println("Data inválida:" + oPessoa.DataValida("51/31/22200"));
+		System.out.println("Data válida:(s)" + oPessoa.DataValida("01/01/2000"));
+		System.out.println("Data válida:(n)" + oPessoa.DataValida("51/31/22200"));
 		System.out.println("Data nascimento anterior ao dia atual(s): " + oPessoa.DataDeNascimentoAnteriorADiaAtual());
 		System.out.println("Ja fez aniversario: (s)" + oPessoa.JaFezAniversario());
 		
 		oPessoa.SetData("01/01/2030");
 		System.out.println("Ja fez aniversario: (n)" + oPessoa.JaFezAniversario());
-		System.out.println("Data nascimento anterior ao dia atual(não)" + oPessoa.DataDeNascimentoAnteriorADiaAtual());
+		System.out.println("Data nascimento anterior ao dia atual(n)" + oPessoa.DataDeNascimentoAnteriorADiaAtual());
 		System.out.println("Hoje é aniversario: (n)" + oPessoa.HojeEhAniversario());
 		
 		oPessoa.setDataNascimento(new Date());
